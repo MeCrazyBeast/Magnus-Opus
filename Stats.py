a6fb17bb2f5ee7f8e777cabae006049d51a9aa3b
IAteNoodles <90202919+IAteNoodles@users.noreply.github.com>
2021-09-07 12:32:25 +0530

    Update Stats.py

--- Stats.py
+++ Stats.py
@@ -19,8 +19,8 @@ class Stats:
             self.STAMINA * self.STRENGTH)) * race_object * self.ADDITIONAL_MULTIPLIER
         self.MAGICAL_MULTIPLIER = (math.log(
             self.STAMINA * self.INTELLIGENCE)) * race_object * self.ADDITIONAL_MULTIPLIER
-        self.CRITICAL_HIT_RATE=1
-        self.CRITICAL_DAMAGE=1+race_object*self.LEVEL_MULTIPLIER*self.PHYSICAL_MULTIPLIER*self.MAGICAL_MULTIPLIER/100
+        self.CRITICAL_HIT_RATE = 1
+        self.CRITICAL_DAMAGE = 1 + race_object * self.LEVEL_MULTIPLIER * self.PHYSICAL_MULTIPLIER * self.MAGICAL_MULTIPLIER / 100
         self.EXPERIENCE_POINTS_REQUIRED = math.pow(math.e, math.log(self.LEVEL)) + self.LEVEL * self.LEVEL_MULTIPLIER
 
     def display_stats(self):
@@ -30,12 +30,14 @@ class Stats:
         print("Agility: ", self.AGILITY)
 
     def check_critical(self):
-        if random.random(100)<math.pi*self.CRITICAL_HIT_RATE:
+        if random.randrange(0, 100) < math.pi * self.CRITICAL_HIT_RATE:
             return True
         return False
 
-    def atk(self):
+    def atk(self, mode=None):
         if self.check_critical():
+            return self.STRENGTH * self.CRITICAL_DAMAGE * self.PHYSICAL_MULTIPLIER, True
+        return self.STRENGTH * self.PHYSICAL_MULTIPLIER, False
 
 
 user = Stats(100, 150, 128, 86, 18)
